6bc52a45b6f6659a101f8461a7087bd294db7f85
Sgechuki <sgechuki@outlook.com>
2024-02-25 12:18:21 -0800

    Count number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,17 @@ def get_status():
     """Retrieve statistics of the
     number of objects for each type."""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_count():
+    """retrieves the number of each objects by type"""
+    count = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(count)
