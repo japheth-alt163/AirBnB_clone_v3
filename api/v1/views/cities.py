f5d44fcb110a8bcf3b61f889d80aac517da78029
Sgechuki <sgechuki@outlook.com>
2024-02-26 12:29:24 -0800

    handles all default RESTFul API actions for City

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,8 @@ from models.state import State
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities_by_state(state_id):
     """
     Retrieves the list of all City objects of a State
@@ -45,7 +46,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """
     Creates a City
